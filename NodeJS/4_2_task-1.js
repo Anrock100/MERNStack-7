06c8ace3ebb832cce7709fa74354349ea6a0ae5a
Krishna Aryal <krishna.dreamersit@gmail.com>
2022-06-27 19:11:52 +0545

    June 27 2022

--- NodeJS/4_2_task-1.js
+++ NodeJS/4_2_task-1.js
@@ -1,7 +1,26 @@
 // import http, url, qurystring
 var http = require('http');
+var url = require('url');
+
 //create server
 var server = http.createServer((req, res)=>{
+    var objUrl = url.parse(req.url, true);
+    //console.log(objUrl);
+    var command = objUrl.pathname; //receive /sum
+    var objQuery = url.parse(objUrl.href, {parseQueryString: true}).query;
+    //console.log(objQuery);
+    var n1 = Number(objQuery.num1); //value of num1
+    var n2 = Number(objQuery.num2); //value of num2
+    var n3 = null;
+    // console.log(command, n1, n2);
+    if(command==='/sum'){
+        n3 = n1+n2;
+    }
+    else if(command ==='/sub'){
+        n3 = Math.abs(n1+n2);
+    }
+    res.write("Result : "+n3);
+    //append n1, n2, n3, command, and current datetime on file as json data.
     res.end("Server is running....");
 });
 server.listen(80, "127.0.0.1");
