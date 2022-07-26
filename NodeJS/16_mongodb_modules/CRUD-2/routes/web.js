bf5cda9a6085de319c4d7b4d284113cbde5304a2
Krishna Aryal <krishna.dreamersit@gmail.com>
2022-07-26 18:30:29 +0545

    july 26 2022

--- NodeJS/16_mongodb_modules/CRUD-2/routes/web.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-import {homeController, showLoginController, doLoginController} from '../controllers/homeController.js';
-
-router.get('/', homeController);
-router.get('/showLogin', showLoginController);
-router.get('/getLogin', doLoginController)
-export default router;
