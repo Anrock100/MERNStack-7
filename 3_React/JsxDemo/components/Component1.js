58ac0885e4bb899c7ab1f047bd3c7e37d8246341
Krishna Aryal <krishna.dreamersit@gmail.com>
2022-08-10 18:29:46 +0545

    Aug 10 2022

---
+++ 3_React/JsxDemo/components/Component1.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import MyTag1 from './jsxes/Component1.jsx';
+
+class Components extends Component {
+    render() {
+        return (
+            <MyTag1 />
+        );
+    }
+}
+export default Components;
