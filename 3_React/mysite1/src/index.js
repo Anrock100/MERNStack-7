d5c84cc42abbd0ab1c9badf719a8b9595b398b2c
Krishna Aryal <krishna.dreamersit@gmail.com>
2022-08-29 18:32:41 +0545

    Aug 29 2022

---
+++ 3_React/mysite1/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+// FOR EXECUTE / RUN:
+const rootElement = document.getElementById("root");
+ReactDOM.render(<div>Hello</div>, rootElement);
